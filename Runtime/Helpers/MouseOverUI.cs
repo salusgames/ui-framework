1592f12f6d65cc63b2dd3195b7af6e4bcd7d5d1f
Dan Wilkins <dwilkins@salusgames.com>
2022-01-30 14:35:22 +0000

    Added class to check if mouse pointer is over UI

---
+++ Runtime/Helpers/MouseOverUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SalusGames.UIFramework.Helpers
+{
+    public static class MouseOverUI
+    {
+        public static bool MouseIsOver()
+        {
+            var eventDataCurrentPosition = new PointerEventData(EventSystem.current) {position = Input.mousePosition};
+            var results = new List<RaycastResult>();
+
+            EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+
+            return results.Count > 0;
+        }
+    }
+}
